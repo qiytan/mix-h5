a7b98ccb9d1a26b6f5a1b491196b3309cd2b9daa
高鸿 <979956869@qq.com>
2018-04-09 21:59:15 +0800

    home
    ziya

---
+++ home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  str="123";
+  items = [];
+  constructor(public navCtrl: NavController) {
+    for (let i = 0; i < 30; i++) {
+      this.items.push( this.items.length );
+    }
+  }
+  goSub(){
+    this.navCtrl.push(LoginPage,{name:'wkx'});
+  }
+  setTimeout(() => {
+    for (let i = 0; i < 30; i++) {
+      this.items.push( this.items.length );
+    }
+
+    console.log('Async operation has ended');
+    infiniteScroll.complete();
+  }, 500);
+}
+}
